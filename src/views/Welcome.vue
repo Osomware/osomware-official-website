ffb15212b2cfa936406a4ea5801ebec2fbc59a1e
Joshua Galit <joshuaimalay@gmail.com>
2020-11-16 21:32:11 +0800

    Import Scroll Indicator and ScrollToTop vue component

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="welcome">
+    <scroll-indicator />
     <v-container>
       <the-nav-bar />
       <v-main>
-        
+        <hero-section />
       </v-main>
+      <scroll-to-top />
     </v-container>
   </div>
 </template>
@@ -13,7 +15,10 @@
   export default {
     name: 'welcome',
     components: {
-      TheNavBar: () => import('@/components/layouts/TheNavBar')
+      TheNavBar: () => import('@/components/layouts/TheNavBar'),
+      HeroSection: () => import('@/components/sections/HeroSection'),
+      ScrollToTop: () => import('@/components/sections/ScrollToTop'),
+      ScrollIndicator: () => import('@/components/sections/ScrollIndicator')
     }
   }
 </script>
