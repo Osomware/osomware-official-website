e24d279863b107ff7e7b7869ecf922179230c54e
Joshua Galit <joshuaimalay@gmail.com>
2020-11-18 10:20:11 +0800

    Designed OurVision Section with team group images

---
+++ src/components/sections/OurVisionSection.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+    <v-layout class="our-vision-section">
+      <v-row align="center" justify="center">
+        <v-col cols="12">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="6">
+              <div class="text-capitalize font-weight-black blueprimary--text mb-2">Our Vision</div>
+              <div class="display-2 font-weight-black pb-6 darkblue--text title-img">
+               Meet Our Awesome Team
+              </div>
+              <div class="darkblue--text text-justify description">
+                Need an entire development team, maybe and additional <br>
+                development unit or just small supplemental development team <br>
+                for special projects that are outside of your core business?  if so,
+                consider a dedicated team from Osomware
+                <br>
+                <br>
+                You can ramp up in a hurry by leveraging the methodologies
+              </div>
+              <v-hover v-slot="{ hover }">
+                <v-btn color="primary"
+                       class="mt-5 text-capitalize font-weight-medium rounded-lg"
+                       large
+                       depressed
+                       :outlined="hover ? true : false">
+                  Learn More <v-icon right>mdi-arrow-right</v-icon>
+                </v-btn>
+              </v-hover>
+            </v-col>
+            <v-col cols="12" md="6" class="d-none d-md-flex">
+              <v-img
+                src="@/assets/img/team.png"
+                class="d-block ml-auto mr-auto"
+                max-width="600"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-layout>
+  </section>
+</template>
+
+<style scoped>
+  .our-vision-section {  
+    min-height: 100vh;
+  }
+  .description {
+    line-height: 2.5;
+    opacity: 80% !important;
+  }
+  .title-img {
+    background-image: url('./../../assets/svgs/circles.svg');
+    background-repeat: no-repeat;
+  }
+</style>
