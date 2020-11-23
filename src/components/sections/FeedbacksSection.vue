dbe21118b41ec3a9b526d4b913d5d57f21ced188
Joshua Galit <joshuaimalay@gmail.com>
2020-11-23 10:53:56 +0800

    Changed busines title and logo

--- src/components/sections/FeedbacksSection.vue
+++ src/components/sections/FeedbacksSection.vue
@@ -2,11 +2,23 @@
   <section>
     <v-layout class="our-feedbacks-section">
       <v-row align="center" justify="center">
-        <v-col cols="12" class="text-center">
+        <v-col class="text-center">
           <div class="text-capitalize font-weight-medium blueprimary--text mb-2">Feedbacks</div>
           <div class="display-2 font-weight-black darkblue--text title-img py-4">
             Our client appreciate what we do
           </div>
+          <!-- <v-card flat class="mx-auto" max-width="900">
+            <v-system-bar lights-out></v-system-bar>
+            <v-carousel>
+              <v-carousel-item
+                v-for="(item,i) in items"
+                :key="i"
+                :src="item.src"
+                reverse-transition="fade-transition"
+                transition="fade-transition"
+              ></v-carousel-item>
+            </v-carousel>
+          </v-card> -->
         </v-col>
       </v-row>
     </v-layout>
