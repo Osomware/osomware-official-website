7740763088292043c8b38f440f08a47829161c9d
Joshua Galit <joshuaimalay@gmail.com>
2020-11-23 11:07:13 +0800

    Import vue-carousel-3d

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import Carousel3d from 'vue-carousel-3d'
 
 Vue.config.productionTip = false
+Vue.use(Carousel3d)
 
 new Vue({
   router,
